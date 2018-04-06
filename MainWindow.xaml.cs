fa1150525ec12b34baa6c7528c3ca41af947bcb8
Constantine Philipchenko <const-phi@ya.ru>
2018-04-06 13:06:25 +0300

    Calculator demo.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using FirstGuiDemo.Tricks;
+using static System.Math;
+
 namespace FirstGuiDemo
 {
     /// <summary>
@@ -20,28 +23,128 @@ namespace FirstGuiDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Button Accept;
+        private Dictionary<string, Func<double, double, double>> operations;
+
+        private StringBuilder buffer = new StringBuilder();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            Accept = new Button
+            operations = new Dictionary<string, Func<double, double, double>>
             {
-                Content = "Ok",
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center
+                {"+", (x, y) => x + y}, 
+                {"-", (x, y) => x - y}, 
+                {"*", (x, y) => x * y}, 
+                {"/", (x, y) => x / y}, 
+                {"^", Pow}, 
             };
-            Accept.Click += ButtonBase_OnClick;
+
+            var digitsGrid = new Grid();
+
+            int colSize = 3;
+            for (int i = 0; i < colSize; i++)
+                digitsGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            int digitBase = 10;
+            int rowSize = digitBase / colSize + 1;
+            for (int i = 0; i < rowSize; i++)
+                digitsGrid.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0; i < digitBase; i++)
+            {
+                var tmp = new Button
+                {
+                    Content = i.ToString("D"),
+                    FontFamily = new FontFamily("Monospace"),
+                    FontSize = 18
+                };
+                tmp.Click += DigitOnClick;
+                tmp.AddTo(digitsGrid, i / colSize, i % colSize);
+            }
+
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition {MinWidth = 50});
+            MainGrid.RowDefinitions.Add(new RowDefinition());
+            MainGrid.RowDefinitions.Add(new RowDefinition { MinHeight = 150});
+            MainGrid.RowDefinitions.Add(new RowDefinition());
 
 
-            MainGrid.Children.Add(Accept);
+            digitsGrid.AddTo(MainGrid, row: 1, rowSpan: 2);
+
+            var operationPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left,
+            };
+            
+            foreach (var operation in operations)
+            {
+                var button = new Button
+                {
+                    Content = operation.Key,
+                    FontFamily = new FontFamily("Monospace"),
+                    FontSize = 18,
+                    Width = 50,
+                };
+                button.Click += OperationOnClick;
+                button.AddTo(operationPanel);
+            }
+
+            operationPanel.AddTo(MainGrid, row: 1, column: 1);
+
+            var result = new Button
+            {
+                Content = "Result"
+            };
+            result.Click += ResultOnClick;
+            result.AddTo(MainGrid, row: 2, column: 1);
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private void ResultOnClick(object sender, RoutedEventArgs routedEventArgs)
         {
-            MessageBox.Show("Hello world!", "Local title", 
-                MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+            var tmp = buffer.ToString();
+
+            var nodes = tmp.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                // WARNING!!!
+                var first = double.Parse(nodes[0]);
+                var operation = nodes[1];
+                var second = double.Parse(nodes[2]);
+
+                var result = operations[operation](first, second).ToString("F");
+                MessageBox.Show($"result of [{first} {operation} {second}] is {result}");
+                // WARNING!!!
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OperationOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var sourceButton = sender as Button;
+            if (sourceButton == null) return;
+
+            var key = sourceButton.Content.ToString();
+            MessageBox.Show(key);
+
+            buffer.AppendFormat($" {key} ");
+        }
+
+        private void DigitOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var sourceButton = sender as Button;
+            if (sourceButton == null) return;
+
+            var digit = sourceButton.Content.ToString();
+            if (digit.All(char.IsDigit))
+                MessageBox.Show(digit);
+
+            buffer.Append(digit);
         }
     }
 }
---
+++ Tricks/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FirstGuiDemo.Tricks
+{
+    public static class ExtensionMethods
+    {
+        public static void AddTo(this UIElement element, Panel panel,  
+            int row = 0, int column = 0,
+            int rowSpan = 1, int columnSpan = 1)
+        {
+            Grid.SetRow(element, row);
+            Grid.SetRowSpan(element, rowSpan);
+            Grid.SetColumn(element, column);
+            Grid.SetColumnSpan(element, columnSpan);
+            panel.Children.Add(element);
+        }
+    }
+}
